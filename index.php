5f4e20357b16c8fd4dad25df464ba079864330d8
GoesDey <gusdekmh7778@gmail.com>
2025-03-03 23:40:12 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum hic eum qui laudantium odit, ut sunt vero, ab eaque, minima corrupti at consequatur? Asperiores, sapiente tempora. Facere ipsum amet voluptate!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
